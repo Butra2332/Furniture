b8e7981e9b72c277a9ee58c29696671db5cc8196
Antoxa <bytrik2332@tut.by>
2021-04-26 23:30:57 +0300

    finish file  contact

--- src/js/script.js
+++ src/js/script.js
@@ -5,23 +5,41 @@ $(function () {
     $('.rightside-menu__close').on('click', function() {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
-    $(document).ready(function(){
-        $('.carousel__slider').slick({
-            dots: true,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 500,
-            arrows: false,
-        });
+    $('.carousel__slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+
+    $('.contact-slider').slick({ 
+        dots: true, 
+        slidesToShow: 1,
+        slideToScroll: 1,
+        arrows: false
     });
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
         }
     });
+    
     $('[data-fancybox]').fancybox({
         protect: true
-    });
+    });   
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
